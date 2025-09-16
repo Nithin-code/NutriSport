090c4c8afc17a4ce31f012aec5a4300a0bf901f6
Nithin_Dell_G15 <shanmukhsainithin@gmail.com>
2025-09-17 00:07:54 +0530

    admin panel added

--- feature/admin/src/commonMain/kotlin/com/nithin/admin/AdminPannel.kt
+++ feature/admin/src/commonMain/kotlin/com/nithin/admin/AdminPannel.kt
@@ -1,18 +1,28 @@
 package com.nithin.admin
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.nithin.admin.components.ProductCard
+import com.nutrisport.shared.BorderIdle
+import com.nutrisport.shared.ButtonPrimary
 import com.nutrisport.shared.FontSize
 import com.nutrisport.shared.IconPrimary
 import com.nutrisport.shared.Resources
@@ -33,7 +43,7 @@ fun AdminPanelScreen(
             TopAppBar(
                 title = {
                     Text(
-                        text = "Aadmin panel",
+                        text = "Admin panel",
                         fontFamily = UbuntuMediumFont(),
                         fontSize = FontSize.LARGE,
                         color = TextPrimary
@@ -63,6 +73,31 @@ fun AdminPanelScreen(
                     }
                 }
             )
+        },
+        floatingActionButton = {
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                color = ButtonPrimary,
+                border = BorderStroke(
+                    width = 1.dp,
+                    color = BorderIdle
+                ),
+                shadowElevation = 5.dp,
+                onClick = {
+
+                }
+            ) {
+                Box(modifier = Modifier
+                    .size(56.dp),
+                    contentAlignment = Alignment.Center
+                ){
+                    Icon(
+                        modifier = Modifier.size(24.dp),
+                        painter = painterResource(Resources.Icon.Plus),
+                        contentDescription = "Add icon"
+                    )
+                }
+            }
         }
     ) { padding->
 
@@ -70,14 +105,18 @@ fun AdminPanelScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(
-                    top = padding.calculateTopPadding(),
+                    top = padding.calculateTopPadding() + 12.dp,
                     bottom = padding.calculateBottomPadding()
-                )
+                ).padding(horizontal = 12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
 
-            item {
-                ProductCard()
+            repeat(7){
+                item {
+                    ProductCard()
+                }
             }
+
 
         }
 
